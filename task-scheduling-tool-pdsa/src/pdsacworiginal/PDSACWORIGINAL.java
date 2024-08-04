56b60b2a0cb2810265b415e3a4ce614214632dcc
ShashiKalum11 <COHNDSE241F-120@student.nibm.lk>
2024-08-04 13:51:32 +0530

    TaskSchedulingToolPdsa.Java and PDSACWORIGINAL.Java

---
+++ task-scheduling-tool-pdsa/src/pdsacworiginal/PDSACWORIGINAL.java
@@ -0,0 +1,14 @@
+
+package pdsacworiginal;
+
+import pdsacworiginal.Menu;
+import pdsacworiginal.PriorityQueue;
+
+public class PDSACWORIGINAL {
+
+    public static void main(String[] args) {
+       PriorityQueue pq = new PriorityQueue();
+        Menu m = new Menu();
+    }
+    
+}
